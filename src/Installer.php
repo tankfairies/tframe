873cf0f335e8e47c00ad45598d9c0d48ebdf7d59
Jon <jon@tankfairies.com>
2019-08-19 23:48:11 +0100

    adding echo

--- src/Installer.php
+++ src/Installer.php
@@ -9,6 +9,7 @@ class Installer
 
     public static function postPackageInstall(Event $event)
     {
+        echo "running posrt install\n";
         $installedPackage = $event->getComposer()->getPackage();
         
         mkdir('testpath');
